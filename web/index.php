30feb234dfca8a4d88154b3946021b16f3673b64
CPekko <eirik_fosse2@hotmail.com>
2015-09-27 14:17:41 +0200

    Disallow embedding (iframe) on other pages to prevent clickjacking

--- web/index.php
+++ web/index.php
@@ -10,8 +10,15 @@ if (! extension_loaded('openssl')) {
 session_cache_limiter(false);
 session_start();
 
-if(! isset($_SESSION['request_counter']))
+if (! isset($_SESSION['request_counter']))
     $_SESSION['request_counter'] = 0;
+
+if (! isset($_SESSION['CSRF_token'])){
+	$_SESSION['CSRF_token'] = md5(uniqid(rand(), true));
+}
+
+# Disallow embedding. All iframes etc. will be blank, or contain a browser specific error page.
+header("Content-Security-Policy: frame-ancestors 'none'");
 
 if (preg_match('/\.(?:png|jpg|jpeg|gif|txt|css|js)$/', $_SERVER["REQUEST_URI"]))
     return false; // serve the requested resource as-is.
